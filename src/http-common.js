dafe771a8d34d380b95a4e142a8515ad8ee46cdd
Ryan Lima <ryanlima08021997@gmail.com>
2020-07-11 00:40:04 -0300

    Deploy Grades Front

---
+++ src/http-common.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+//Define a URL base da origem para consumo do servico
+export default axios.create({
+	baseURL: process.env.API_URL || "http://localhost:3003/",
+	headers: {
+		"Content-type": "application/json",
+	},
+});
